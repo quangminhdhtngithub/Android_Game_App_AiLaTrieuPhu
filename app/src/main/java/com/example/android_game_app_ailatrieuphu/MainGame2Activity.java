25842153f5f7d0d3a8fa9fd991e5232d3ad1c972
quangminhdhtngithub <ID+tango4567@users.noreply.github.com>
2020-09-06 16:15:45 +0700

    first commit | Tao cac giao dien cho app

---
+++ app/src/main/java/com/example/android_game_app_ailatrieuphu/MainGame2Activity.java
@@ -0,0 +1,14 @@
+package com.example.android_game_app_ailatrieuphu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainGame2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_game2);
+    }
+}
